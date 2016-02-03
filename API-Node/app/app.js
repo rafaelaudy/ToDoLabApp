5f579dc7798687978269a50f3d0b226e81d12c75
Rafael Audy Glanzner <rafael.audy@gmail.com>
2016-02-03 18:22:54 +0000

    Parsing json objects before it gets to the APIs

--- API-Node/app/app.js
+++ API-Node/app/app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var toDoRoute = require('./routes/toDoRoute');
+
+app.use(bodyParser.json());
 
 app.use('/todo', toDoRoute);
 
