f17d47b21d78d8f216eff06fa6ccbcfeee61908a
Rafael Audy Glanzner <rafael.audy@gmail.com>
2016-02-03 21:41:44 +0000

    Improve readbility of the to do route

--- API-Node/app/routes/toDoRoute.js
+++ API-Node/app/routes/toDoRoute.js
@@ -1,21 +1,25 @@
 var router = require('express').Router();
 var toDoRepository = require('../repositories/toDoRepository');
 
-router.get('/', function (req, res) {
-    res.json(toDoRepository.get());
-});
-
-router.get('/numberCompletedGlobally', function (req, res) {
-    res.json(toDoRepository.getGlobalTotal());
-});
-
-router.post('/', function (req, res) {
-    res.json(toDoRepository.add(req.body))
-});
-
-router.put('/:code', function (req, res) {
-    req.body.id = req.params.code;
-    res.json(toDoRepository.update(req.body));
-});
+setUpRoutes();
 
 module.exports = router;
+
+function setUpRoutes () {
+    router.get('/', function (req, res) {
+        res.json(toDoRepository.get());
+    });
+
+    router.get('/numberCompletedGlobally', function (req, res) {
+        res.json(toDoRepository.getGlobalTotal());
+    });
+
+    router.post('/', function (req, res) {
+        res.json(toDoRepository.add(req.body))
+    });
+
+    router.put('/:code', function (req, res) {
+        req.body.id = req.params.code;
+        res.json(toDoRepository.update(req.body));
+    });
+}
