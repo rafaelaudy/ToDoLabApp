da8e2fe05b2209b6f3f1cd37332b614d4ebcba03
Rafael.Glanzner <rafaelglanzner@RyanWalders-MacBook-Pro.local>
2016-02-05 23:15:52 +0000

    Setting up protractor and grunt test command

--- UI-Angular/gruntfile.js
+++ UI-Angular/gruntfile.js
@@ -1,14 +1,25 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         'http-server': {
-            'dev': {
+            dev: {
                 root: 'app',
                 openBrowser: true
+            },
+            test: {
+                root: 'app',
+                runInBackground: true
             }
+        },
+
+        exec: {
+            webDriverUpdate: 'webdriver-manager update',
+            runFunctionalTests: 'protractor test/functional/protractor.conf.js'
         }
     });
 
-    grunt.registerTask('default', ['http-server']);
-    grunt.loadNpmTasks('grunt-http-server');
-};
+    grunt.registerTask('start', ['http-server:dev']);
+    grunt.registerTask('test', ['http-server:test', 'exec:webDriverUpdate', 'exec:runFunctionalTests']);
 
+    grunt.loadNpmTasks('grunt-http-server');
+    grunt.loadNpmTasks('grunt-exec');
+};
---
+++ UI-Angular/test/functional/domain/toDo/toDoSpec.js
@@ -0,0 +1,23 @@
+describe('Given a to do API', function () {
+    describe('when adding', function () {
+        it('should save the to do', function () {
+        });
+   });
+
+   describe('when updating', function () {
+        it('should update the to do', function () {
+        });
+
+        it('should return an 400 error if the id is not provided', function () {
+        });
+   });
+
+   describe('when querying', function () {
+        it('should return a list of to dos', function () {
+        });
+
+        it('should return the number of todos globally completed', function () {
+        });
+    });
+});
+
